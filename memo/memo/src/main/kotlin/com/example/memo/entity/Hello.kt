64d33f0d7484f02e032c86b6ecb63aeee9dfd09a
jkjan <jkjan@naver.com>
2020-01-08 16:56:43 +0900

    jpa, junit, spring boot, mysql

---
+++ memo/memo/src/main/kotlin/com/example/memo/entity/Hello.kt
@@ -0,0 +1,11 @@
+package com.example.memo.entity
+
+import org.hibernate.annotations.GenericGenerator
+import javax.persistence.*
+
+@Entity
+class Hello(
+        @Id
+        @Column(name = "id")
+        val id : String= "def",
+        val point : Int = 0)
---
+++ memo/memo/src/main/kotlin/com/example/memo/service/HelloService.kt
@@ -0,0 +1,15 @@
+package com.example.memo.service
+
+import com.example.memo.entity.Hello
+import com.example.memo.repository.HelloRepository
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.stereotype.Service
+
+@Service
+class HelloService(private val helloRepository: HelloRepository) {
+    fun createHello (hello : Hello) = helloRepository.save(hello)
+    fun readHello(): MutableList<Hello> = helloRepository.findAll()
+    fun updateHello(hello: Hello) = createHello(hello)
+    fun deleteHello (id : String) = helloRepository.deleteById(id)
+    fun deleteAllHello() = helloRepository.deleteAll()
+}
