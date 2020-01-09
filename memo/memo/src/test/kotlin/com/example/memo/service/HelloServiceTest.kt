277b0eb4b0bd66848e0fee1e32f4f732d395ae09
jkjan <jkjan@naver.com>
2020-01-09 17:59:37 +0900

    jpa, junit, spring boot, mysql, apo

--- memo/memo/src/test/kotlin/com/example/memo/service/HelloServiceTest.kt
+++ memo/memo/src/test/kotlin/com/example/memo/service/HelloServiceTest.kt
@@ -12,19 +12,19 @@ import org.junit.jupiter.api.Assertions.assertEquals
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest
-class DemoApplicationTests(@Autowired private val helloRepository: HelloRepository) {
-	@Test
-	fun createTest() {
-		val hello = Hello("hello", 100)
-		helloRepository.save(hello)
-		val test = helloRepository.findAll()[0]
-		assertEquals(test.point, 100)
-	}
+class HelloServiceTest(@Autowired private val helloRepository: HelloRepository) {
+    @Test
+    fun createTest() {
+        val hello = Hello("hello", 100)
+        helloRepository.save(hello)
+        val test = helloRepository.findAll()[0]
+        assertEquals(test.point, 100)
+    }
 
-	@Test
-	fun deleteAllTest() {
-		helloRepository.deleteAll()
-		val helloList = helloRepository.findAll()
-		assertEquals(helloList.size, 0)
-	}
+    @Test
+    fun deleteAllTest() {
+        helloRepository.deleteAll()
+        val helloList = helloRepository.findAll()
+        assertEquals(helloList.size, 0)
+    }
 }
